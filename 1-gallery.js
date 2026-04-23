8f1a559330ef6f6eed30f0f6b5d76a790a96497b
levsvitlana13 <lev.svitlana13@gmail.com>
2026-04-23 09:02:21 +0000

    Deploying to gh-pages from @ levsvitlana13/goit-js-hw-09@27137a604faf46759fd219a13161db80c568c2c8 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,12 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Gallery");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as t}from"./assets/vendor-CgTBfC_f.js";const e=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Haulage Freight"}],r=document.querySelector(".gallery"),p=e.map(({preview:a,original:i,description:o})=>`
+      <li class="gallery-item">
+        <a class="gallery-link" href="${i}">
+          <img
+            class="gallery-image"
+            src="${a}"
+            alt="${o}"
+          />
+        </a>
+      </li>
+    `).join("");r.innerHTML=p;new t(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"});
 //# sourceMappingURL=1-gallery.js.map
